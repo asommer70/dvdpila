7825270749f30a653ae0490c5cbf6f933f51ac16
Adam Sommer <adam@thehoick.com>
2017-04-18 11:30:28 -0400

    Have image preview working for Dvd form.

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -1,3 +1,28 @@
 $(document).foundation();
 
 console.log('high from app.js...');
+
+// Display the image to be uploaded.
+$('#dvd_image').on('change', function(e) {
+  readURL(this);
+});
+
+function readURL(input) {
+  //
+  // Read the contents of the image file to be uploaded and display it.
+  //
+  if (input.files && input.files[0]) {
+    reader = new FileReader();
+
+    reader.onload = function(e) {
+      $('.image_to_upload').attr('src', e.target.result);
+      $preview = $('.preview');
+
+      if ($preview.hasClass('hide')) {
+        $preview.toggleClass('hide');
+      }
+    }
+
+    reader.readAsDataURL(input.files[0]);
+  }
+}
