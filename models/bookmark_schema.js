80da75761811c54f09f12aeeb10d5de0299dfce4
Adam Sommer <adam@thehoick.com>
2017-09-17 09:48:46 -0400

    Removed Node files.

--- models/bookmark_schema.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-const Schema = mongoose.Schema;
-
-const BookmarkSchema = new Schema({
-  name: { type: String, required: [true, 'Name is required.'] },
-  time: Number,
-}, { timestamps: true });
-
-module.exports = BookmarkSchema;
--- models/tag_schema.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-const Schema = mongoose.Schema;
-
-const TagSchema = new Schema({
-  name: { type: String, required: [true, 'Name is required.'] },
-}, { timestamps: true });
-
-module.exports = TagSchema;
