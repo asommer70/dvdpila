285be76b83c78d3f5b513eb9e6fc07fd4ae89afe
Adam Sommer <adam@thehoick.com>
2017-09-21 15:29:38 -0400

    Have CRUD tests for Dvds complete.

--- models/dvd_test.go
+++ models/dvd_test.go
@@ -18,7 +18,7 @@ func init() {
 }
 
 
-func TestCreateDvd(t *testing.T) {
+func TestCreateDeleteDvd(t *testing.T) {
 	dvd := Dvd{
 		Title: "Ghost In The Shell",
 		Rating: 5,
@@ -37,6 +37,40 @@ func TestCreateDvd(t *testing.T) {
 
 	if (dt.Title != "Ghost In The Shell") {
 		t.Errorf("Expected dt.Title to be 'Ghost In The Shell' but it is %v", dt.Title)
+	}
+
+	ldb.Delete(&dt)
+
+	var dd Dvd
+	ldb.First(&dd)
+
+	if (dd.Title != "") {
+		t.Errorf("Expected dd to be nil but it is not...")
+	}
+}
+
+func TestUpdateDvd(t *testing.T) {
+	dvd := Dvd{
+		Title:        "Ghost In The Shell",
+		Rating:       5,
+		AbstractTxt:  `In the near future, the vast majority of humans are [[Human enhancement|augmented]] with cybernetics, enhancing various traits like vision, strength, and intelligence. Hanka Robotics, the world's leading developer of augmentative technology, establishes a secret project to develop a mechanical body, or "shell", that can [[Brain–computer interface|integrate]] a human brain rather than an [[artificial intelligence|AI]]. `,
+		AbstractSrc:  "Wikipedia",
+		AbstractURL:  "https://en.wikipedia.org/wiki/Ghost_in_the_Shell_(2017_film)",
+		ImageURL:     "http://dvdpila/images/posters/Ghost_in_the_Shell_2017_film.png",
+		FileURL:      "http://videos/Ghost_In_The_Shell_2017.mkv",
+		PlaybackTime: 0,
+	}
+
+	ldb.Create(&dvd)
+
+	dvd.PlaybackTime = 100
+	ldb.Save(&dvd)
+
+	var dt Dvd
+	ldb.First(&dt, dvd.ID)
+
+	if (dt.PlaybackTime != 100) {
+		t.Errorf("Expected dt.PlaybackTime to be 100 but it is %v", dt.PlaybackTime)
 	}
 
 	ldb.Delete(&dt)
