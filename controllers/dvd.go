cca435584b5e6ab7e696d39490991f27086ad63b
Adam Sommer <adam@thehoick.com>
2017-09-24 06:31:20 -0400

    Fixed method definitions for Dvd Controller, setup templates to be passed to controller, and created a test index template.

--- controllers/dvd.go
+++ controllers/dvd.go
@@ -4,35 +4,39 @@ import (
 	"net/http"
 	"github.com/julienschmidt/httprouter"
 	"github.com/jinzhu/gorm"
+	"html/template"
 )
 
 type DvdController struct{
 	db *gorm.DB
+	tpl *template.Template
 }
 
-func NewDvdController(db *gorm.DB) *DvdController {
-	return &DvdController{db}
+func NewDvdController(db *gorm.DB, tpl *template.Template) *DvdController {
+	return &DvdController{db, tpl}
 }
 
-func (dc DvdController) Find(res http.ResponseWriter, req http.Request, p httprouter.Params) {
-	// Find user by ID p.byName("id").
+func (dc DvdController) Find(res http.ResponseWriter, req *http.Request, params httprouter.Params) {
+	// Find Dvd by ID param.byName("id").
 
 	// If no "id" parameter return all.
 
 	// Or return none.
+	//fmt.Fprintf(res, "I've Got Worms...")
+	dc.tpl.ExecuteTemplate(res, "index.gohtml", nil)
 }
 
-func (dc DvdController) Create(res http.ResponseWriter, req http.Request, p httprouter.Params) {
+func (dc DvdController) Create(res http.ResponseWriter, req *http.Request, params httprouter.Params) {
 
 }
 
-func (dc DvdController) Update(res http.ResponseWriter, req http.Request, p httprouter.Params) {
+func (dc DvdController) Update(res http.ResponseWriter, req *http.Request, params httprouter.Params) {
 	// Find user by ID p.byName("id").
 
 	// Update the Dvd.
 }
 
-func (dc DvdController) Delete(res http.ResponseWriter, req http.Request, p httprouter.Params) {
+func (dc DvdController) Delete(res http.ResponseWriter, req *http.Request, params httprouter.Params) {
 	// Find user by ID p.byName("id").
 
 	// Delete the Dvd.
--- main.go
+++ main.go
@@ -2,8 +2,28 @@ package main
 
 import (
 	"log"
+	"github.com/julienschmidt/httprouter"
+	"dvdpila/controllers"
+	"dvdpila/db"
+	"net/http"
+	"html/template"
 )
+
+var tpl *template.Template
+
+func init() {
+	tpl = template.Must(template.ParseGlob("templates/*"))
+}
 
 func main() {
 	log.Println("Executing DVD Pila!...")
+	db := db.Connect()
+	dc := controllers.NewDvdController(db, tpl)
+
+	router := httprouter.New()
+	router.GET("/", dc.Find)
+	//router.Handle("GET", "/", dvdController.Find)
+
+	log.Println("Listening on port 3000...")
+	http.ListenAndServe("localhost:3000", router)
 }
