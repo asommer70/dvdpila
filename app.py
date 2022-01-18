80168a3c948ea8940424fd0d05fa8056b109832f
Adam Sommer <asommer70@gmail.com>
2022-01-18 16:28:46 -0500

    Setup migrations for the databse.

--- app.py
+++ app.py
@@ -1,8 +1,9 @@
 from dotenv import load_dotenv
 from flask import Flask
+from flask_debugtoolbar import DebugToolbarExtension
+from flask_migrate import Migrate
 from flask import url_for
 from flask import render_template
-from flask_debugtoolbar import DebugToolbarExtension
 import os
 from models import db, Dvd, Episode, Bookmark, Tag
 
@@ -18,6 +19,7 @@ db_pass = os.getenv('DB_PASS')
 db_con = f"postgresql+psycopg2://{db_user}:{db_pass}@localhost/dvdpila"
 app.config['SQLALCHEMY_DATABASE_URI'] = db_con
 db.init_app(app)
+migrate = Migrate(app, db)
 
 
 # import pdb; pdb.set_trace()
